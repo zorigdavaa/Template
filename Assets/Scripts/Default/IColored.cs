b5df451f8d6dd05c92e6ca9f1d92c8df5820ed11
zorigdavaa <zorigoo.nexus@yahoo.com>
2024-05-22 12:55:20 +0800

    ss

---
+++ Assets/Scripts/Default/IColored.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IColored
+{
+    public Color GetColor();
+    public void SetColor(Color color);
+}
